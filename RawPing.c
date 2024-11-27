f74580a157f3dd9847a9f7e79fa72b97fcd99226
Mark Ballereau <mark.b@live.fr>
2024-11-27 17:49:05 +0100

    Add IP and ICMP header filling functions

--- RawPing.c
+++ RawPing.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
 
 void display(char *buffer, int length) {
     for (int i = 0; i < length; i+=2) {
@@ -17,15 +21,34 @@ void display(char *buffer, int length) {
     }
 }
 
-int main() {
-    // Exemple d'utilisation avec les 50 premiers entiers
-    char buffer[50];
-    for (int i = 0; i < 50; i++) {
-        buffer[i] = i;
-    }
+// Remplit l'en-tête IP avec les valeurs spécifiées
+void fill_ip_header(struct ip *ip_header, size_t payload_size, u_short id, u_char ttl, const char *src_ip, const char *dest_ip) {
+    memset(ip_header, 0, sizeof(struct ip));
 
-    // Appel de la fonction pour afficher les octets
-    display(buffer, 50);
+    ip_header->ip_hl = sizeof(struct ip) / 4;
+    ip_header->ip_v = 4;
+    ip_header->ip_tos = 0;
+    ip_header->ip_len = htons(sizeof(struct ip) + payload_size);
+    ip_header->ip_id = htons(id);
+    ip_header->ip_off = htons(0);
+    ip_header->ip_ttl = ttl;
+    ip_header->ip_p = IPPROTO_ICMP;
+    ip_header->ip_sum = htons(0);
+    ip_header->ip_src.s_addr = inet_addr(src_ip);
+    ip_header->ip_dst.s_addr = inet_addr(dest_ip);
+}
+
+// Remplit l'en-tête ICMP avec les valeurs spécifiées
+void fill_icmp_header(struct icmp *icmp_header, u_short id, u_short sequence) {
+    memset(icmp_header, 0, sizeof(struct icmp));
     
+    icmp_header->icmp_type = ICMP_ECHO;
+    icmp_header->icmp_code = 0;
+    icmp_header->icmp_cksum = htons(0);
+    icmp_header->icmp_id = htons(id);
+    icmp_header->icmp_seq = htons(sequence);
+}
+
+int main() {
     return 0;
 }
